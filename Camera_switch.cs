c30e230adad30818711d7cd706a9896c25baa0df
Piotr Martficki <petermax_30@gmx.co.uk>
2015-03-19 18:36:00 +0000

    First_commit
    Complete scripts with my corrections

---
+++ Camera_switch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Camera_switch : MonoBehaviour {
+
+	public Camera MainCamera;
+	public Camera Camera_side;
+
+	// Use this for initialization
+	void Start () {
+		MainCamera.enabled = true;
+		Camera_side.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.C)) {
+			if (MainCamera.enabled == true)
+			{
+				MainCamera.enabled = false;
+				Camera_side.enabled = true;
+			} 
+			else
+			{
+				MainCamera.enabled = true;
+				Camera_side.enabled = false;
+			}
+		}
+	
+	}
+}
