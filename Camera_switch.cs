0aa80cf6edca088aca7ba50cdd09a59fc4c3cbcd
Piotr Martficki <petermax_30@gmx.co.uk>
2015-03-20 21:00:04 +0000

    Camera corrections

--- Camera_switch.cs
+++ Camera_switch.cs
@@ -7,12 +7,14 @@ public class Camera_switch : MonoBehaviour {
 	public Camera MainCamera;
 	public Camera Camera_side;
 	public Camera Camera_ground;
+	public Camera Camera_cockpit;
 
 	// Use this for initialization
 	void Start () {
 		MainCamera.enabled = true;
 		Camera_side.enabled = false;
 		Camera_ground.enabled = false;
+		Camera_cockpit.enabled = false;
 	
 	}
 	
@@ -25,18 +27,28 @@ public class Camera_switch : MonoBehaviour {
 				MainCamera.enabled = false;
 				Camera_side.enabled = true;
 				Camera_ground.enabled = false;
+				Camera_cockpit.enabled = false;
 			} 
 			else if (Camera_side.enabled == true)
 			{
 				MainCamera.enabled = false;
 				Camera_side.enabled = false;
 				Camera_ground.enabled = true;
+				Camera_cockpit.enabled = false;
+			}
+			else if (Camera_ground.enabled == true)
+			{
+				MainCamera.enabled = false;
+				Camera_side.enabled = false;
+				Camera_ground.enabled = false;
+				Camera_cockpit.enabled = true;
 			}
 			else
 			{
 				MainCamera.enabled = true;
 				Camera_side.enabled = false;
 				Camera_ground.enabled = false;
+				Camera_cockpit.enabled = false;
 			}
 		}
 	
